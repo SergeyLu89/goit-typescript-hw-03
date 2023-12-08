12ca21fdfaa2c7004778a9b143282fbd2c941a1b
Sergey Lutsenko <lytsenko.89@gmail.com>
2023-12-08 15:55:32 +0200

    task_04_done

--- src/4.ts
+++ src/4.ts
@@ -1,4 +1,41 @@
+class Key {
+    private signature: number = Math.random();
+    
+    public getSignature(): number {
+        return this.signature;
+    };
+};
 
+class Person {
+    constructor(public key: Key) { };
+
+    public getKey(): Key {
+        return this.key;
+    };
+};
+
+abstract class House {
+    private tenants: Person[] = [];
+    public door: boolean = false;
+
+    constructor(public key: Key) { }
+
+    public comeIn(tenant: Person): void {
+        if (this.door) {
+            this.tenants.push(tenant);
+        };
+    };
+
+    abstract openDoor(info: Key);
+};
+
+class MyHouse extends House {
+     openDoor(info): void {
+        if (info.getSignature() === key.getSignature()) {
+            this.door = true;
+         };
+    };
+};
 
 const key = new Key();
 
